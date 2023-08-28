8779cff67b647567ff2375f3167f2beea9e9482a
zqr <1546975213@qq.com>
2023-08-28 10:10:24 +0800

    added some more functions.

--- cvt_bag_to_tum.py
+++ cvt_bag_to_tum.py
@@ -1,20 +1,37 @@
 import rosbag
 import numpy as np
+import os
 
 # 设置rosbag文件路径和输出文件路径
-bag_path = "/path/to/bag/file.bag"
-output_path = "/path/to/output/file.txt"
+bag_path = "/home/zqr/devel/dataset/evo_bag/"
+
+bag_files = [f for f in os.listdir(bag_path) if f.endswith('.bag')]
+print("找到以下rosbag文件：")
+bag_files.sort()
+# Print out the list of files with numbers for selection
+for i, f in enumerate(bag_files):
+    print(f"\t{i+1}. {f}")
+# Get user input for selection
+selection = int(input("\n选择想要估计的rosbag文件: "))
+# Assign the selected file path to bag_file
+bag_file = os.path.join('/home/zqr/devel/dataset/evo_bag/', bag_files[selection-1])
 
 # 设置需要转换的topic名称
-topic_name = "/odom"
+topic_name = "/Odometry"
+output_path = bag_file[:-4] + ".txt"
 
+print(output_path)
 # 读取rosbag文件
-bag = rosbag.Bag(bag_path)
+bag = rosbag.Bag(bag_file)
 
 # 初始化变量
 timestamps = []
 positions = []
 orientations = []
+
+# 如果没有topic_name报错
+if topic_name not in bag.get_type_and_topic_info()[1].keys():
+    raise ValueError("Topic {} not found in bag file {}".format(topic_name, bag_file))
 
 # 遍历rosbag文件中的消息
 for topic, msg, t in bag.read_messages(topics=[topic_name]):
@@ -33,12 +50,12 @@ orientations = np.array(orientations)
 
 # 将时间戳转换为相对时间
 timestamps = np.array(timestamps)
-timestamps -= timestamps[0]
+# timestamps -= timestamps[0]
 
 # 将位置和姿态信息保存到tum格式的txt文件中
 with open(output_path, "w") as f:
     for i in range(len(timestamps)):
-        f.write("{:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(
+        f.write("{:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(
             timestamps[i] / 1e9,
             positions[i, 0], positions[i, 1], positions[i, 2],
             orientations[i, 0], orientations[i, 1], orientations[i, 2], orientations[i, 3]))
---
+++ kinematic_test.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+vicon_marker_file = "/home/zqr/devel/dataset/evo_bag/kinematic_test/vicon_kuafu_kuafu.tum"
+vicon_foot_file = "/home/zqr/devel/dataset/evo_bag/kinematic_test/vicon_kuafu_fot_kuafu_fot.tum"
+pinocchio_imu_foot_file = "/home/zqr/devel/dataset/evo_bag/kinematic_test/bitbot_foot_position.tum"
+
+vicon_marker_data = np.loadtxt(vicon_marker_file, delimiter=' ')
+vicon_foot_data = np.loadtxt(vicon_foot_file, delimiter=' ')
+pinocchio_imu_foot_data = np.loadtxt(pinocchio_imu_foot_file,delimiter=' ')
+
+
+T_imu_marker = [[1,0,0,0.047],[0,1,0,0.01825],[0,0,1,0.48385],[0,0,0,1]]
+
+vicon_imu_foot = np.zeros((3,vicon_marker_data.shape[0]-1))
+vicon_imu_foot_time = []
+
+for i in range(vicon_marker_data.shape[0]-1):
+    vicon_marker_r = R.from_quat(vicon_marker_data[i, 4:8])
+    vicon_marker_t = vicon_marker_data[i, 1:4]
+    vicon_G_marker = np.vstack((np.hstack((vicon_marker_r.as_matrix(), vicon_marker_t.reshape(3,1))), np.array([0,0,0,1])))
+    vicon_foot_r = R.from_quat(vicon_foot_data[i, 4:8])
+    vicon_foot_t = vicon_foot_data[i, 1:4]
+    vicon_G_foot = np.vstack((np.hstack((vicon_foot_r.as_matrix(), vicon_foot_t.reshape(3,1))), np.array([0,0,0,1])))
+
+    temp1 = np.dot(T_imu_marker, np.linalg.inv(vicon_G_marker))
+    temp2 = np.dot(temp1, vicon_G_foot)
+    vicon_imu_foot[0:3,i] = temp2[0:3,3]
+    vicon_imu_foot_time.append(vicon_marker_data[i,0] - vicon_marker_data[0,0])
+
+vicon_imu_foot_txyz = np.vstack((vicon_imu_foot_time,vicon_imu_foot[0,:], vicon_imu_foot[1,:], vicon_imu_foot[2,:]))
+
+pinocchio_imu_foot_time = []
+for i in range(pinocchio_imu_foot_data.shape[0]):
+    pinocchio_imu_foot_time.append(pinocchio_imu_foot_data[i,0] - pinocchio_imu_foot_data[0,0])
+
+pinocchio_imu_foot_txyz = np.vstack((pinocchio_imu_foot_time,pinocchio_imu_foot_data[:,1], pinocchio_imu_foot_data[:,2], pinocchio_imu_foot_data[:,3]))
+
+# 绘制vicon_imu_foot_txyz的三维图像
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.plot(vicon_imu_foot_txyz[1,:], vicon_imu_foot_txyz[2,:], vicon_imu_foot_txyz[3,:], label='vicon_imu_foot')
+ax.plot(pinocchio_imu_foot_txyz[1,:], pinocchio_imu_foot_txyz[2,:], pinocchio_imu_foot_txyz[3,:], label='pinocchio_imu_foot')
+#将xyz轴写上xyz
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.legend()
+plt.show()
